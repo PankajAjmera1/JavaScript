4809cbbe71b884d6e74a399d6dcf91793732a7c1
PankajAjmera1 <coderajmera@gmail.com>
2023-07-18 20:15:42 +0530

    Start

---
+++ Sign - UP/operaotrs.js
@@ -0,0 +1,21 @@
+//In this we gonna learn about Operators
+// var num1 = 7;
+// var num2 = 9;
+
+// console.log(num1 + num2);
+
+// var result = num1 > num2; //Boolean 
+// console.log(result); 
+
+
+//Use cases -->
+
+// D = (L - S)/L * 100 ;
+var sellingPrice = 199;
+var listingPrice = 799;
+
+var discountPrice = ((listingPrice - sellingPrice)/listingPrice)*100;
+console.log("Discount percentage :"+ discountPrice);
+diplayDiscountPercentage = Math.round(discountPrice)
+
+
