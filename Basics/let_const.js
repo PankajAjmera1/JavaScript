de524df9884073f464b4c1874fcced9d2971b7f1
Pankaj Ajmera <103686473+PankajAjmera1@users.noreply.github.com>
2023-10-30 13:40:57 +0000

    let &  const

---
+++ Basics/let_const.js
@@ -0,0 +1,12 @@
+const accountId=14453
+let accountEmail = "Pankaj@google.com"
+var accountPassword ="123456"
+accountCity ="Mathura"
+// accountId = 2 // not allowed
+accountEmail = "ajmera@gl.com"
+accountPassword="456788"
+
+accountCity ="Bengaluru"
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
